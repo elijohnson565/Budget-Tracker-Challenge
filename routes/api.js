f6ffa7399461a4569b9edeb56b50f0882ef1802e
Elijah Johnson <elijohnson565@gmail.com>
2022-06-03 18:49:47 -0700

    added server.js

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+// Transaction post route
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
@@ -11,6 +12,9 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
+
+// Transaction bulk post route
+
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
@@ -20,6 +24,9 @@ router.post("/api/transaction/bulk", ({body}, res) => {
       res.status(404).json(err);
     });
 });
+
+
+// Transaction get route
 
 router.get("/api/transaction", (req, res) => {
   Transaction.find({}).sort({date: -1})
